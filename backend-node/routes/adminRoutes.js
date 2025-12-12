ceffe788b8384d67b187ce2276a7032f74998449
Mohamed Niyaz A <mohamed.niyaz219@gmail.com>
2025-12-13 03:39:05 +0530

    file structure

--- backend-node/routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const adminController = require('../controllers/adminController');
-const auth = require('../middleware/authMiddleware');
-
-router.post('/', auth, adminController.createTenant);
-
-module.exports = router;
--- backend-node/scripts/migrate-tenants.js
+++
@@ -1,2 +0,0 @@
-// Placeholder: iterate tenants and run Sequelize migrations per schema
-console.log('Run tenant migrations here');
